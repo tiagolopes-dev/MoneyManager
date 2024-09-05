532ca3107d841f5e0033d62b868ce3d26d13f897
tiagolopes-dev <tiagolopesc.dev@gmail.com>
2024-09-05 00:05:12 -0300

    corrigindo novos bugs

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Home } from "./pages/Home";
-import { ModalDelete } from "./components/ModalDelete";
 
 export function Router() {
   return (
@@ -10,7 +9,6 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
-      <Route path="/modal" element={<ModalDelete/>}/>
     </Routes>
   );
 }
